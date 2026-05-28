9f35c65bfa267600cc8759501485c1bba77d0f23
unrandomsurlenet <130355438+unrandomsurlenet@users.noreply.github.com>
2026-05-28 18:31:24 +0200

    Initial commit

--- src/main/java/com/dimproject/registries/DimProjectItems.java
+++ src/main/java/com/dimproject/registries/DimProjectItems.java
@@ -1,34 +1,24 @@
-package com.dimproject.registries
+package com.dimproject.registries;
 
 
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.*;
-import net.minecraft.world.item.equipment.ArmorType;
-import net.minecraftforge.common.ForgeSpawnEggItem;
+
+
+
+import com.dimproject.DimProjectMod;
+
+import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-import java.util.List;
-import java.util.function.Function;
-
 public class DimProjectItems {
-    public static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS, DimProjectMod.MOD_ID);
+	public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, DimProjectMod.MODID);
 
-    public static final RegistryObject<Item> ALEXANDRITE = registerItem("alexandrite",
-            Item::new);
-     
-          
-
-    public static RegistryObject<Item> registerItem(String name, Function<Item.Properties, Item> function) {
-        return ModItems.ITEMS.register(name, () -> function.apply(new Item.Properties()
-                .setId(ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID, name)))));
-    }
+    public static final RegistryObject<Item> SKULREX = ITEMS.register("skulrex",
+            () -> new Item(new Item.Properties()));
+    
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
