c0a574f9c40f00092c0313c576306a8090a64c74
unrandomsurlenet <130355438+unrandomsurlenet@users.noreply.github.com>
2026-05-28 10:31:28 +0200

    Update and rename src/main/java/com/example/examplemod/DimProjectMod.java to src/main/java/com/dimproject/DimProjectMod.java

--- src/main/java/com/dimproject/DimProjectMod.java
+++ src/main/java/com/dimproject/DimProjectMod.java
@@ -29,7 +29,7 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
+@Mod(DimProjectMod.MODID)
 public class DimProjectMod
 {
     // Define mod id in a common place for everything to reference
