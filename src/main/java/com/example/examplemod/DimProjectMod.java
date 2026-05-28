63acbee695f06ac182410ddc2a3c4a8f6a1ea1a3
unrandomsurlenet <130355438+unrandomsurlenet@users.noreply.github.com>
2026-05-28 10:28:36 +0200

    Update and rename ExampleMod.java to DimProjectMod.java

--- src/main/java/com/example/examplemod/DimProjectMod.java
+++ src/main/java/com/example/examplemod/DimProjectMod.java
@@ -30,10 +30,10 @@ import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExampleMod.MODID)
-public class ExampleMod
+public class DimProjectMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "lemod";
+    public static final String MODID = "dimproject";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
