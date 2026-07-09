4db3a17784a8be5cecbde0bda528889d5d7430d4
unrandomsurlenet <130355438+unrandomsurlenet@users.noreply.github.com>
2026-07-09 11:25:20 +0200

    Update DimProjectItems.java

--- src/main/java/com/dimproject/registries/DimProjectItems.java
+++ src/main/java/com/dimproject/registries/DimProjectItems.java
@@ -20,6 +20,8 @@ public class DimProjectItems {
 
     public static final RegistryObject<Item> SKULREX = ITEMS.register("skulrex",
             () -> new SkulrexItem(new Item.Properties()));
+public static final RegistryObject<Item> FOCUS_RETICLE = ITEMS.register("focus_reticle",
+            () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> BOOK = ITEMS.register("book",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> EVANESCENCE = ITEMS.register("evanescence",
